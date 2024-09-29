00a23e0e58fc13600a99702574a604b412625145
saritha28 <53329443+sarithab28@users.noreply.github.com>
2024-09-29 19:40:42 +0530

    feature202 changes by Ethan

--- Application.java
+++ Application.java
@@ -7,6 +7,17 @@ class Main {
     return num * num;
   }
 
+#feature202 changes on reports module by Ethan in oct2023
+public static final Map<String, Class> CLASS_MAP;
+static {
+    Map<String, Class> classMap = new HashMap<>(16);
+    classMap.put("VARCHAR", java.lang.String.class);
+    ...
+    CLASS_MAP = Collections.unmodifiableMap(classMap);
+}
+
+#end of code changes
+
   public static void main(String[] args) {
     int result;
 
